4761ff6842629b81de772ac77fdbb526adb577ca
Peter Koza <peter.koza@ibacz.eu>
2025-01-11 17:42:30 +0100

    mobile nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
-import {ArrowLeft} from "react-feather";
+import {AlignJustify, ArrowLeft, Menu, X} from "react-feather";
 
 export interface HeaderProps {
     title: {
@@ -18,33 +18,45 @@ export interface HeaderProps {
     }>;
 }
 
-const Header: React.FC<HeaderProps> = ({title, backButton, navigation}) =>
-    <header className="bg-white shadow-lg sticky top-0 z-10">
+const Header: React.FC<HeaderProps> = ({title, backButton, navigation}) => {
+    const [isMobileNavOpen, setMobileNavOpen] = useState(false);
+    const toggleMobileNavOpen = () => setMobileNavOpen(!isMobileNavOpen);
+
+    return <header className="bg-white shadow-lg sticky top-0 z-10">
         <div className="grid grid-cols-3 items-center py-4 px-6">
-            {/* Left content*/}
-            <div className="justify-self-start md:pl-4">
+            {/* Back button */}
+            <div className="justify-self-start lg:pl-4">
                 {backButton &&
-                    <Link to={backButton.link} className="text-primary hover:text-secondary flex items-center space-x-2">
-                        <ArrowLeft size={18} />
-                        <span>{backButton.text}</span>
-                    </Link>
+                  <Link to={backButton.link} className="text-primary hover:text-secondary flex items-center space-x-2">
+                    <ArrowLeft size={18}/>
+                    <span>{backButton.text}</span>
+                  </Link>
                 }
             </div>
-            {/* Middle content) */}
+            {/* Title) */}
             <h1 className="text-2xl font-bold text-primary justify-self-center">
                 {title.link ?
                     <a href={title.link}>
-                    {title.text}
+                        {title.text}
                     </a>
                     :
                     title.text
                 }
             </h1>
-            {/* Right content */}
-            <nav className="flex space-x-6 justify-self-end pr-8">
+            {/* Navigation */}
+            <nav className="hidden lg:flex space-x-2 xl:space-x-6  justify-self-end xl:pr-8">
                 {navigation && navigation.map(item => <a href={item.link} key={item.key} className="hover:text-secondary">{item.text}</a>)}
             </nav>
+            {navigation && <>
+                <button className="block lg:hidden text-primary justify-self-end" onClick={toggleMobileNavOpen}>
+                    {isMobileNavOpen ? <X/> : <Menu/>}
+                </button>
+                <nav className={`${isMobileNavOpen ? 'translate-y-0 opacity-100' : '-translate-y-16 opacity-0 pointer-events-none'} md:hidden absolute top-full right-0 bg-white w-auto pl-4 pr-8 rounded-b-md transition-all duration-300 ease-in-out`}>
+                    {navigation && navigation.map(item => <a href={item.link} key={item.key} className="block p-4 hover:text-secondary">{item.text}</a>)}
+                </nav>
+            </>}
         </div>
     </header>
+}
 
 export default Header;
